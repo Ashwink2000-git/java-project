8788883d2221dbf2013519dfa85afea09f73926c
Ashwink2000-git <ashwinkrishna1022@gmail.com>
2024-10-08 16:09:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 # Variables
-APP_NAME=my-java-app # Change this to your application's name (as defined in your pom.xml)
-BUILD_DIR=target     # Maven's default output directory
-UAT_DIR=/var/www/html/$APP_NAME/uat
-PROD_DIR=/var/www/html/$APP_NAME/prod
+APP_NAME=my-java-app  # Change this to match the artifact name in pom.xml
+BUILD_DIR=target      # Maven's default output directory
+UAT_DEPLOY_DIR=/var/lib/jenkins/workspace/my-java-app-uat
 
-# Create deployment directories if they don't exist
-mkdir -p $UAT_DIR
-mkdir -p $PROD_DIR
+# Create deployment directory if it doesn't exist
+mkdir -p $UAT_DEPLOY_DIR
 
 # Copy built JAR file to the UAT directory
-cp $BUILD_DIR/$APP_NAME-*.jar $UAT_DIR/$APP_NAME.jar
+cp $BUILD_DIR/$APP_NAME-*.jar $UAT_DEPLOY_DIR/$APP_NAME.jar
 
 # Restart Nginx to serve the updated application (if necessary)
 systemctl restart nginx
-
-
