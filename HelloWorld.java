85a8e7d780126dc6429a215a41f6162095809dec
Ashwink2000-git <ashwinkrishna1022@gmail.com>
2024-10-08 17:32:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World 6!");
+        System.out.println("Hello, World 8!");
     }
 }
