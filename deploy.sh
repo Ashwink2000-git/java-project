390ff917eca84a18e839b15c81c8e1b012a6a76d
Ashwink2000-git <ashwinkrishna1022@gmail.com>
2024-10-08 15:01:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,15 @@
 
 # Variables
 APP_NAME=hello-world-app
+BUILD_NUMBER=$1  # Get build number from parameter
 UAT_DIR=/var/www/html/$APP_NAME
 
 # Create deployment directory
 mkdir -p $UAT_DIR
 
 # Copy built JAR to UAT directory
-cp target/$APP_NAME-1.0-SNAPSHOT.jar $UAT_DIR/$APP_NAME.jar
+cp target/$APP_NAME-$BUILD_NUMBER-SNAPSHOT.jar $UAT_DIR/$APP_NAME.jar
 
 # Restart Nginx (if necessary)
 # systemctl restart nginx
+
